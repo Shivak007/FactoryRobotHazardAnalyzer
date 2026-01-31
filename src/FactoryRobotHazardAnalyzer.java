fa375a7db30a6eb1a263f2ff2a028ecd10f5a0d9
Shivak007 <kshivaoffl2824@gmail.com>
2026-01-31 10:49:47 +0530

    [Shiva Kumar] Added: UC4: Validate Inputs.

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 /**
  * Factory Robot Hazard Analyzer
- * UC3: Calculate Robot Hazard Risk (No Validation)
+ * UC4: Validate Inputs and Calculate Hazard Risk
  *
  * @author Shiva Kumar
  * @version 1.0
@@ -22,6 +22,15 @@ public class FactoryRobotHazardAnalyzer {
         System.out.print("Enter Machinery State (Worn/Faulty/Critical): ");
         String machineryState = sc.nextLine();
 
+        if (armPrecision < 0.0 || armPrecision > 1.0) {
+            System.out.println("Error: Arm precision must be between 0.0 and 1.0");
+            return;
+        }
+
+        if (workerDensity < 1 || workerDensity > 20) {
+            System.out.println("Error: Worker density must be between 1 and 20");
+            return;
+        }
         double risk = ((1.0 - armPrecision) * 15.0) + (workerDensity * 3.0);
 
         System.out.println("Robot Hazard Risk Score: " + risk);
