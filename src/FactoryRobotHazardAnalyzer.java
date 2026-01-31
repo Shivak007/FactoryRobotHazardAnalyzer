031aa7772f6d8400556bc87d035ae8c085c50f96
Shivak007 <kshivaoffl2824@gmail.com>
2026-01-31 10:53:21 +0530

    [Shiva Kumar] Added: UC5: Refactor Logic into Method.

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 /**
  * Factory Robot Hazard Analyzer
- * UC4: Validate Inputs and Calculate Hazard Risk
+ * UC5: Refactor Risk Calculation into Method
  *
  * @author Shiva Kumar
  * @version 1.0
@@ -9,7 +9,6 @@ import java.util.Scanner;
 public class FactoryRobotHazardAnalyzer {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter Arm Precision (0.0 - 1.0): ");
@@ -22,17 +21,24 @@ public class FactoryRobotHazardAnalyzer {
         System.out.print("Enter Machinery State (Worn/Faulty/Critical): ");
         String machineryState = sc.nextLine();
 
-        if (armPrecision < 0.0 || armPrecision > 1.0) {
-            System.out.println("Error: Arm precision must be between 0.0 and 1.0");
-            return;
-        }
-
-        if (workerDensity < 1 || workerDensity > 20) {
-            System.out.println("Error: Worker density must be between 1 and 20");
-            return;
-        }
-        double risk = ((1.0 - armPrecision) * 15.0) + (workerDensity * 3.0);
+        double risk = calculateHazardRisk(armPrecision, workerDensity, machineryState);
 
         System.out.println("Robot Hazard Risk Score: " + risk);
+    }
+
+    public static double calculateHazardRisk(double ap, int wd, String ms) {
+
+        if (ap < 0.0 || ap > 1.0) {
+            System.out.println("Error: Arm precision must be between 0.0 and 1.0");
+            return -1;
+        }
+
+        if (wd < 1 || wd > 20) {
+            System.out.println("Error: Worker density must be between 1 and 20");
+            return -1;
+
+        }
+
+        return ((1.0 - ap) * 15.0) + (wd * 3.0);
     }
 }
