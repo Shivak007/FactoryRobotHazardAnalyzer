31b5d417e810f82ef539cf514690b2da42e59943
Shivak007 <kshivaoffl2824@gmail.com>
2026-01-31 10:59:27 +0530

    [Shiva Kumar] Added: UC7: Machinery Risk Mapping.

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 /**
  * Factory Robot Hazard Analyzer
- * UC6: Use Custom Exception for Validation
+ * UC7: Machinery State Risk Mapping
  *
  * @author Shiva Kumar
  * @version 1.0
  */
 public class FactoryRobotHazardAnalyzer {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(System.in);
 
@@ -33,14 +33,28 @@ public class FactoryRobotHazardAnalyzer {
     public static double calculateHazardRisk(double ap, int wd, String ms)
             throws RobotSafetyException {
 
-        if (ap < 0.0 || ap > 1.0) {
+        if (ap < 0.0 || ap > 1.0)
             throw new RobotSafetyException("Error: Arm precision must be between 0.0 and 1.0");
+
+        if (wd < 1 || wd > 20)
+            throw new RobotSafetyException("Error: Worker density must be between 1 and 20");
+
+        double factor;
+        switch (ms) {
+            case "Worn":
+                factor = 1.3;
+                break;
+            case "Faulty":
+                factor = 2.0;
+                break;
+            case "Critical":
+                factor = 3.0;
+                break;
+            default:
+                throw new RobotSafetyException("Error: Unsupported machinery state");
         }
 
-        if (wd < 1 || wd > 20) {
-            throw new RobotSafetyException("Error: Worker density must be between 1 and 20");
-        }
-        return ((1.0 - ap) * 15.0) + (wd * 3.0);
+        return ((1.0 - ap) * 15.0) + (wd * factor);
     }
 }
 
