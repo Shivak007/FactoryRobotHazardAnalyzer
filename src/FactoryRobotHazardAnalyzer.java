ef71d61dc03473f3d9ca0b8621553be4e95830f1
Shivak007 <kshivaoffl2824@gmail.com>
2026-01-31 10:56:07 +0530

    [Shiva Kumar] Added: UC6: Custom RobotSafetyException.

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,44 +1,51 @@
 import java.util.Scanner;
 /**
  * Factory Robot Hazard Analyzer
- * UC5: Refactor Risk Calculation into Method
+ * UC6: Use Custom Exception for Validation
  *
  * @author Shiva Kumar
  * @version 1.0
  */
 public class FactoryRobotHazardAnalyzer {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Arm Precision (0.0 - 1.0): ");
-        double armPrecision = sc.nextDouble();
+        try {
+            Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Worker Density (1 - 20): ");
-        int workerDensity = sc.nextInt();
-        sc.nextLine();
+            System.out.print("Enter Arm Precision (0.0 - 1.0): ");
+            double armPrecision = sc.nextDouble();
 
-        System.out.print("Enter Machinery State (Worn/Faulty/Critical): ");
-        String machineryState = sc.nextLine();
+            System.out.print("Enter Worker Density (1 - 20): ");
+            int workerDensity = sc.nextInt();
+            sc.nextLine();
 
-        double risk = calculateHazardRisk(armPrecision, workerDensity, machineryState);
+            System.out.print("Enter Machinery State (Worn/Faulty/Critical): ");
+            String machineryState = sc.nextLine();
 
-        System.out.println("Robot Hazard Risk Score: " + risk);
+            double risk = calculateHazardRisk(armPrecision, workerDensity, machineryState);
+            System.out.println("Robot Hazard Risk Score: " + risk);
+
+        } catch (RobotSafetyException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public static double calculateHazardRisk(double ap, int wd, String ms) {
+    public static double calculateHazardRisk(double ap, int wd, String ms)
+            throws RobotSafetyException {
 
         if (ap < 0.0 || ap > 1.0) {
-            System.out.println("Error: Arm precision must be between 0.0 and 1.0");
-            return -1;
+            throw new RobotSafetyException("Error: Arm precision must be between 0.0 and 1.0");
         }
 
         if (wd < 1 || wd > 20) {
-            System.out.println("Error: Worker density must be between 1 and 20");
-            return -1;
-
+            throw new RobotSafetyException("Error: Worker density must be between 1 and 20");
         }
-
         return ((1.0 - ap) * 15.0) + (wd * 3.0);
+    }
+}
+
+class RobotSafetyException extends Exception {
+    public RobotSafetyException(String message) {
+        super(message);
     }
 }
